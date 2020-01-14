909ea42d178c88a1969fc89873fb557e6242620f
krisnalach <55798623+krisnalach@users.noreply.github.com>
2020-01-14 09:42:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ PLAY_SCREEN = (TOP_LEFT_X, TOP_LEFT_Y, PLAY_WIDTH, PLAY_HEIGHT)
 SQSZ = PLAY_WIDTH / 15
 MINESTOTAL = 40
 flag_count = MINESTOTAL
+PLAY_W_LEFT = SCREEN_WIDTH - PLAY_WIDTH
+PLAY_W_RIGHT = SCREEN_WIDTH - PLAY_WIDTH + PLAY_WIDTH
+PLAY_H_TOP = SCREEN_HEIGHT - PLAY_HEIGHT
+PLAY_H_BOT = SCREEN_HEIGHT - PLAY_HEIGHT + PLAY_HEIGHT
+bigfont = pygame.font.SysFont('symbola', 80)
+smallfont = pygame.font.SysFont('symbola', 45)
+
 
 #main class for each individual square
 class squares():
@@ -119,23 +126,31 @@ def clear_adjacent(win, grid):
                 if i != 0:
                     if grid[i-1][j].type == 'safe' and grid[i-1][j].prox == 0:
                         grid[i-1][j].status = 'clicked'
-                        if i > 1:
-                            grid[i-2][j].status = 'clicked' 
+                        if i > 1 and 14 > j > 0:
+                            grid[i-2][j].status = 'clicked'
+                            grid[i-2][j+1].status = 'clicked'
+                            grid[i-2][j-1].status = 'clicked' 
                 if i != 14: 
                     if grid[i+1][j].type == 'safe' and grid[i+1][j].prox == 0:
                         grid[i+1][j].status = 'clicked'
-                        if i < 13:
-                            grid[i+2][j].status = 'clicked'                    
+                        if i < 13 and 14 > j > 0:
+                            grid[i+2][j].status = 'clicked'  
+                            grid[i+2][j+1].status = 'clicked'
+                            grid[i+2][j-1].status = 'clicked'                  
                 if j != 0: 
                     if grid[i][j-1].type == 'safe' and grid[i][j-1].prox == 0:
                         grid[i][j-1].status = 'clicked'
-                        if j > 1:
+                        if j > 1 and 14 > i > 0:
                             grid[i][j-2].status = 'clicked'
+                            grid[i+1][j-2].status = 'clicked'
+                            grid[i-1][j-2].status = 'clicked'
                 if j != 14: 
                     if grid[i][j+1].type == 'safe' and grid[i][j+1].prox == 0:
                         grid[i][j+1].status = 'clicked'
-                        if j < 13:
+                        if j < 13 and 14 > i > 0:
                             grid[i][j+2].status = 'clicked'
+                            grid[i+1][j+2].status = 'clicked'
+                            grid[i-1][j+2].status = 'clicked'
 
 #gives flag status and image to box passed through
 def flag(box):
@@ -162,7 +177,6 @@ def clicked(win, box, boxes, grid):
     if box.status == 'none' and box.type == 'mine':
         box.image = mine
         box.status = 'clicked'
-        game_over(win, boxes)
     return(grid)  
     
 #checks if all mines have been flagged, if so, player wins
@@ -176,19 +190,23 @@ def win_con(boxes):
         return True
     else:
         return False
-            
-#checks if a bomb has been clicked and shows end game screen                
-def game_over(win, boxes):
+
+#check if player has lost
+def lose_con(boxes):
     for box in boxes:
-        if box.type == 'mine':
-            box.image = mine
-    draw_end(win, 'Game Over', 30, BLACK)
-    button(win, 'Retry', 30, BLACK, 600, 450, 100, 50, main(win))
+            if box.status == 'clicked':
+                if box.type == 'mine':
+                    return True
+                          
+def update_flag(win):
+    flags = 'Flags: %d' % (flag_count)
+    pygame.draw.rect(win, WHITE, (TOP_LEFT_X+PLAY_WIDTH-95, TOP_LEFT_Y-45, 30,30))
+    draw_text(win, flags, 28, BLACK, TOP_LEFT_X+PLAY_WIDTH-95, TOP_LEFT_Y-45)
 
 #shows win screen
 def you_win(win):
     draw_end(win, 'You Won!', 30, BLACK)
-    button(win, 'Retry', 30, BLACK, main(win))
+    #button(win, 'Retry', 30, BLACK, main(win))
 
 #runs through all boxes and updates their image according to their status
 def update_sq(win, boxes):
@@ -209,7 +227,10 @@ def show_info(win):
     bombs = 'Mines: %d' % (MINESTOTAL)
     pygame.draw.rect(win, WHITE, (TOP_LEFT_X, TOP_LEFT_Y-45, 30,30))
     draw_text(win, bombs, 28, BLACK,  TOP_LEFT_X, TOP_LEFT_Y-45)
-
+    flags = 'Flags: %d' % (flag_count)
+    pygame.draw.rect(win, WHITE, (TOP_LEFT_X+PLAY_WIDTH-95, TOP_LEFT_Y-45, 30,30))
+    draw_text(win, flags, 28, BLACK, TOP_LEFT_X+PLAY_WIDTH-95, TOP_LEFT_Y-45)
+    
 #basic method to draw text
 def draw_text(surface, text, size, color, x, y):
     font = pygame.font.SysFont('symbola', size, bold=False)
@@ -224,18 +245,49 @@ def draw_end(surface, text, size, color):
     pygame.draw.rect(surface, WHITE, (TOP_LEFT_X +PLAY_WIDTH /2 - (label.get_width()/2) - 10, TOP_LEFT_Y + PLAY_HEIGHT/2 - label.get_height()/2 - 10 + 350, label.get_width()+20, label.get_height()+20))
     surface.blit(label,(TOP_LEFT_X +PLAY_WIDTH /2 - (label.get_width()/2), TOP_LEFT_Y + PLAY_HEIGHT/2 - label.get_height()/2 + 350))
 
-#draws a button that performs given action when pressed
-def button(surface, text, size, color, x, y, w, h, action = None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    font = pygame.font.SysFont('symbola', size, bold=True) 
+#method to draw text in center of screen
+def draw_text_middle(surface, text, size, color):
+    font = pygame.font.SysFont('symbola', size, bold=True)
     label = font.render(text, 1, color)
-    pygame.draw.rect(surface, BLACK, (x,y, w, h))
-    pygame.draw.rect(surface, WHITE, (x+5 , y+5, w-10, h-10))
-    surface.blit(label,(x+15,y+15))
-    if x < mouse[0] < x+w and y < mouse[1] < y+w:
-        if click[0] == 1 and action != None:
-            action()
+    surface.blit(label,(TOP_LEFT_X +PLAY_WIDTH /2 - (label.get_width()/2), TOP_LEFT_Y + PLAY_HEIGHT/2 - label.get_height()/2))
+
+#method to show reset button and reset game if clicked
+def play_again(win, words):
+
+    text = bigfont.render(words, 13, (0, 0, 0))
+    textx = SCREEN_WIDTH / 2 - text.get_width() / 2
+    texty = SCREEN_HEIGHT / 2 - text.get_height() / 2
+    textx_size = text.get_width()
+    texty_size = text.get_height()
+
+    pygame.draw.rect(display, BLACK, ((textx - 10, texty - 10),
+                                               (textx_size + 20, texty_size +
+                                                20)))
+
+    pygame.draw.rect(display, WHITE, ((textx - 5, texty - 5),
+                                               (textx_size + 10, texty_size +
+                                                10)))
+
+    display.blit(text, (SCREEN_WIDTH / 2 - text.get_width() / 2,
+                       SCREEN_HEIGHT / 2 - text.get_height() / 2))
+
+    clock = pygame.time.Clock()
+    pygame.display.flip()
+    in_main_menu = True
+    while in_main_menu:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                in_main_menu = False
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                if x >= textx - 5 and x <= textx + textx_size + 5:
+                    if y >= texty - 5 and y <= texty + texty_size + 5:
+                        in_main_menu = False
+                        main(win)
+                        break
+
 
 #main game method
 def main(win):
@@ -246,6 +298,7 @@ def main(win):
     draw_squares(win, grid)
     boxes = []
     pygame.display.update()
+    flag_count = MINESTOTAL
 
     #puts all square objects in 'grid' into a string called boxes
     #having all objects in a list rather than a 2D array helps iterate through it better
@@ -262,24 +315,29 @@ def main(win):
     #main game loop
     running = True
     while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-
+                
                 #get mouse pos and if clicked
                 click = pygame.mouse.get_pressed()
                 x, y = pygame.mouse.get_pos()
                 
                 #readjusting mouse because for some reason it was off centered
                 x = x-SQSZ
-                y = y-SQSZ
-
+                y = y-SQSZ 
+            
                 #if right mouse button is clicked, put it in list and pass flag method through all objects in list
                 flagged_sq = [box for box in boxes if click[2] and x<box.x<x+SQSZ and y<box.y<y+SQSZ]
-                for box in flagged_sq:
+                for count, box in enumerate(flagged_sq):
                     flag(box)
+                    flag_count = flag_count - count
 
+                
+            
                 #if left mouse button is clicked, put it in list and pass clicked method through all objects in list
                 clicked_sq = [box for box in boxes if click[0] and x<box.x<x+SQSZ and y<box.y<y+SQSZ]
                 for box in clicked_sq:
@@ -288,35 +346,49 @@ def main(win):
                 #if middle mouse button is clicked, put it in list and pass unflag method through all objects in list
                 unflagged_sq = [box for box in boxes if click[1] and x<box.x<x+SQSZ and y<box.y<y+SQSZ]
                 for box in unflagged_sq:
-                    unflag(box)    
+                    unflag(box)      
 
         #constantly updates status of boxes near clicked boxes
         clear_adjacent(win, grid)
 
-        #constantly checks if player won and updates game
+
+        #constantly checks if player won/lost and updates game
         if win_con(boxes):
-            you_win(win)
+            play_again(win, 'You Won! Play Again?')
+        update_sq(win, boxes)
+
+        if lose_con(boxes):
+            play_again(win, 'You Lost! Try Again?')
+            for box in boxes:
+                if box.type == 'mine':
+                    box.image == 'mine'
         update_sq(win, boxes)
 
         clock.tick(60)
         pygame.display.update()
         
-#def main_menu(win):
-    #running = True
-    #while running:
-        #win.fill(BLACK)
-        #draw_text_middle(win, 'Enter any to key to begin', 60, WHITE)
-        #pygame.display.update()
-        #for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-                #running = False
-            #if event.type == pygame.KEYDOWN:
-                #main(win)
+def main_menu(win):
+    running = True
+    while running:
+        win.fill(BLACK)
+        draw_text_middle(win, 'PRESS ANY KEY TO BEGIN', 60, WHITE)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                main(win)
             
 
 #initializing screen and applying caption
 display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Minesweeper')
-
+pygame.display.set_icon(icon)
 #runs game
-main(display)
+main_menu(display)
+
+
+#TODO
+#show all mines upon loss
+#make it so you can't just flag every box to win
+
