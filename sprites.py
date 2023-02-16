cc59132f40f7735b42c9cf97aa72e5fb3cd592c1
Krisna Lach <lach.krisna@gmail.com>
2023-02-15 20:54:38 -0500

    Working on making things neater. IP.

--- sprites.py
+++ sprites.py
@@ -1,22 +1,31 @@
 import pygame
 import os
 
-one = pygame.image.load(os.path.join('sprites/one.png'))
-two = pygame.image.load(os.path.join('sprites/two.png'))
-three = pygame.image.load(os.path.join('sprites/three.png'))
-four = pygame.image.load(os.path.join('sprites/four.png'))
-five = pygame.image.load(os.path.join('sprites/five.png'))
-six = pygame.image.load(os.path.join('sprites/six.png'))
-seven = pygame.image.load(os.path.join('sprites/seven.png'))
-eight = pygame.image.load(os.path.join('sprites/eight.png'))
-empty = pygame.image.load(os.path.join('sprites/empty.png'))
-block = pygame.image.load(os.path.join('sprites/block.png'))
-flagged = pygame.image.load(os.path.join('sprites/flagged.png'))
-mine = pygame.image.load(os.path.join('sprites/mine.png'))
+#getting images (?) 
+def load_images():
+    image_names = ["one", "two", "three", "four", "five", "six", "seven", "eight", "empty", "block", "flagged", "mine", "minesweep"]
+    var_names = 
+
+    one = pygame.image.load('sprites/one.png')
+    two = pygame.image.load('sprites/two.png')
+    three = pygame.image.load('sprites/three.png')
+    four = pygame.image.load('sprites/four.png')
+    five = pygame.image.load('sprites/five.png')
+    six = pygame.image.load('sprites/six.png')
+    seven = pygame.image.load('sprites/seven.png')
+    eight = pygame.image.load('sprites/eight.png')
+    empty = pygame.image.load('sprites/empty.png')
+    block = pygame.image.load('sprites/block.png')
+    flagged = pygame.image.load('sprites/flagged.png')
+    mine = pygame.image.load('sprites/mine.png')
+    icon = pygame.image.load('sprites/minesweep.png')
+
 
 def format(image):
     return pygame.transform.scale(image, (40,40))
 
+#reformatting so it fits in game screen
+def format_all()
 one = format(one)
 two = format(two)
 three = format(three)
@@ -29,5 +38,6 @@ empty = format(empty)
 block = format(block)
 flagged = format(flagged)
 mine = format(mine)
+icon = format(icon)
 
 nums = [one, two, three, four, five, six, seven, eight] 
